974b51efc0a8845b3541c22ac9b304c8c81817f0
Kassamba Hussein Diaby <khuss063@gmail.com>
2018-02-27 09:27:32 +0100

    Console, lib, solution

---
+++ Isen.Dotnet.Library/Hello.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Isen.Dotnet.Library
+{
+    //renvoie des salutations
+    public class Hello
+    {
+        public static string Greet(string name){
+            string message = "Hello " +name;
+            return message;
+        }
+    }
+}
